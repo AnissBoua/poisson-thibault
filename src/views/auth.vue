a6ca89df41b5a15ee26f2fb6e1ac4005c4aea0d4
Anisse <anissbouainbi@hotmail.it>
2024-03-13 11:29:26 +0100

    fix token

--- src/views/auth.vue
+++ src/views/auth.vue
@@ -1,25 +1,11 @@
 <template>
-    <!-- component -->
-<!-- Container -->
-<div class="flex flex-col h-screen bg-gray-100">
-    <!-- Auth Card Container -->
+<div class="flex flex-col h-screen">
     <div class="grid place-items-center mx-2 my-20 sm:my-auto">
-        <div class="flex">
-            <span class="text-center font-bold my-20 mx-auto">        
-                <a href="https://egoistdeveloper.github.io/twcss-to-sass-playground/" target="_blank" class="text-blue-600">
-                    Connexion
-                </a>
-            </span>
-        </div>
-    
-    
-        <!-- Auth Card -->
         <div class="w-11/12 p-12 sm:w-8/12 md:w-6/12 lg:w-5/12 2xl:w-4/12 
             px-6 py-10 sm:px-10 sm:py-6 
-            bg-white rounded-lg shadow-md lg:shadow-lg">
+            bg-neutral-700 rounded-lg shadow-md lg:shadow-lg">
 
-            <!-- Card Title -->
-            <h2 class="text-center font-semibold text-3xl lg:text-4xl text-gray-800">
+            <h2 class="text-center font-semibold text-3xl lg:text-4xl">
                 Login
             </h2>
 
@@ -83,35 +69,16 @@ export default {
   },
   methods: {
     login() {
-
-            accountService.login({
-                username: this.username,
-                password: this.password
-            })
-    // ici on charge les données de connexion
-                .then(res => {
-                   let token= accountService.saveToken(res.data.access_token)
-                    this.$router.push('/')
-                    console.log(token);
-                })
-                .catch(err => console.log(err))
-//                    fetch('http://127.0.0.1:8000', {
-//                 headers: {
-//                     'Accept': 'application/json',
-//                     'Content-Type': 'application/json'
-//                 },
-//                 method: 'POST',
-//                 body: JSON.stringify(this.user)
-//             })
-//                 .then(blob => blob.json())
-//                 .then(data => {
-//                     console.log(data)
-//                     localStorage.setItem('token', data.access_token)
-//                     this.$router.push('/admin/dashboard')
-//                 })
-//                 .catch(err => console.log(err))
-//       // Ici vous pouvez gérer la soumission du formulaire, par exemple envoyer les données au serveur
-        }
+        accountService.login({
+            username: this.username,
+            password: this.password
+        })
+        .then(res => {
+            let token= accountService.saveToken(res.data.access)
+            this.$router.push('/')
+        })
+        .catch(err => console.log(err))
+    }
   }
 };
 </script>
